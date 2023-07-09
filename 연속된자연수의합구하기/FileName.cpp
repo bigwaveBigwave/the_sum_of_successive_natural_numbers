8800a491cc599c0dfaf70d22966f673e98ece630
bigwaveBigwave <gb6ke3@naver.com>
2023-07-09 23:34:41 +0900

    초기 커밋.

---
+++ 연속된자연수의합구하기/FileName.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+    int count = 1; // 경우의 수(=우리가 원하는 결과) 본인 자신을 미리 카운트
+    int start_index = 1;
+    int end_index = 1;
+    int sum = 1;
+    while (end_index != N) // end_index == N 인 경우(본인자신_를 제외, count의 초기값이 1인 이유
+    {
+        // 작성하세요.
+        if (sum == N) {
+            count++;
+            //star_idx,end_idx -> sum
+            end_index++;
+            sum = sum + end_index;
+
+        }
+        else if (sum > N) {
+            sum = sum - start_index;
+            start_index++;
+
+        }
+        else { // sum <N
+            end_index++;
+            sum = sum + end_index;
+        }
+    }
+    cout << count << "\n";
+}
