85f9fa60815c0d2cc2b0b874f9469f256e0d7c7a
bigwaveBigwave <gb6ke3@naver.com>
2023-07-19 23:56:14 +0900

    sum > N

--- 연속된자연수의합구하기/FileName.cpp
+++ 연속된자연수의합구하기/FileName.cpp
@@ -14,6 +14,7 @@ int main()
     int start_index = 1;
     int end_index = 1;
     int sum = 1;
+     
     while (end_index != N) // end_index == N 인 경우(본인자신_를 제외, count의 초기값이 1인 이유
     {
         
@@ -27,6 +28,7 @@ int main()
         else if (sum > N) {
             sum = sum - start_index;
             start_index++;
+
 
         }
         else { // sum < N
